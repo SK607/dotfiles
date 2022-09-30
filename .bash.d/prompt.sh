d70f58e7f9f0837be7e0935262e2fca916022b16
Sergey Konkin <sergey.konkin.eth@gmail.com>
2022-09-30 14:50:06 +0300

    improved bash prompt

--- .bash.d/prompt.sh
+++ .bash.d/prompt.sh
@@ -1,52 +1,67 @@
-if [ -n "$PROMPT_NERD_ENABLED" ]; then
-    ICON_SSH=' '
-    ICON_CHUSER=' '
-    ICON_GIT=' '
-    ICON_WD=' '
-    ICON_PY=' '
-fi
+# ███████╗██╗  ██╗  BASH PROMPT
+# ██╔════╝██║ ██╔╝    ┌── ⇅ → user@host *2 gitbranch:dir  
+# ███████╗█████╔╝     └─> |  
+# ╚════██║██╔═██╗   
+# ███████║██║  ██╗  Sergey Konkin 
+# ╚══════╝╚═╝  ╚═╝  https://github.com/SK607
 
-red="\[\e[00;38;5;1m\]"
-green="\[\e[0;38;5;2m\]"
-yellow="\[\e[0;38;5;3m\]"
-blue="\[\e[0;38;5;4m\]"
-magenta="\[\e[0;38;5;5m\]"
-teal="\[\e[0;38;5;6m\]"
-black="\[\e[00;38;5;8m\]"
-reset="\[\e[m\]"
 
-__prompt_icons (){
-    local icons=''
-    if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-        icons+="${ICON_SSH:-S}"
+__bash_prompt() {
+    ## COLORS
+    local black="\[\e[00;38;5;0m\]"
+    local red="\[\e[00;38;5;1m\]"
+    local green="\[\e[0;38;5;2m\]"
+    local yellow="\[\e[0;38;5;3m\]"
+    local blue="\[\e[0;38;5;4m\]"
+    local magenta="\[\e[0;38;5;5m\]"
+    local teal="\[\e[0;38;5;6m\]"
+    local white="\[\e[2;38;5;7m\]"
+    local reset="\[\e[m\]"
+
+    local prompt=''
+    ## LINE 1
+    prompt+='\n'
+
+    # arrow
+    prompt+="$white┌── "
+
+    # ssh/ login status
+    if [[ -n "$SSH_CLIENT" ]] || [[ -n "$SSH_TTY" ]]; then
+        prompt+="$white⇅ "
     fi
-    if [ -n "$SUDO_USER" ]; then
-        icons+="${ICON_CHUSER:-U}"
+    if [[ -n "$SUDO_USER" ]]; then
+        prompt+="$white→ "
     fi
-    echo -e "$icons"
+
+    # user & host
+    prompt+="$white$USER@$HOSTNAME "
+
+    # git
+    if [[ "$PWD" != "$OLDPWD" ]]; then
+        if [[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = 'true' ]]; then
+            local branch="$(git rev-parse --abbrev-ref HEAD)"
+            local uncommitted="*$(git status --porcelain | wc -l) "
+            prompt+="$green$uncommitted$branch:"
+        fi
+    fi
+
+    # directory
+    prompt+="$blue$(basename $PWD)"
+  
+    # python env
+    if [[ -x "$VIRTUAL_ENV" ]]; then
+        prompt+= "$magenta$(basename $VIRTUAL_ENV) "
+    fi
+
+    ## LINE 2
+    prompt+='\n'
+
+    # arrow
+    prompt+="$white└─> $reset"
+
+    export PS1=$prompt
 }
 
-__prompt_pyenv (){
-    if ! [ -z ${VIRTUAL_ENV+x} ]; then
-        echo -e " $ICON_PY$(basename "$VIRTUAL_ENV")"
-    else
-        echo ''
-    fi
-}
+[[ "$VIRTUAL_ENV_DISABLE_PROMPT" != '1' ]] && export VIRTUAL_ENV_DISABLE_PROMPT='1'
+export PROMPT_COMMAND=__bash_prompt
 
-! [ -x "$(command -v __git_ps_1)" ] && source /usr/share/git/git-prompt.sh
-
-# git rev-parse --git-dir --is-inside-git-dit --is-bar-repository --is-inside-work-tree --short HEAD
-# git describe --contains --all head
-
-PS1="$black┌───"' $(__prompt_icons)'
-PS1+="$black\u"
-PS1+=" $blue$ICON_WD\W"
-PS1+="$green"'$(__git_ps1 " $ICON_GIT%s")'  # TODO: simplify git 
-PS1+="$magenta"'$(__prompt_pyenv)'
-PS1+="\n$black└─> $reset"
-
-unset red green yellow blue magenta teal black reset
-
-export PS1
-export -f __prompt_pyenv __prompt_icons
