6531fa28364406346f6cc4ae22a8099e802fbec4
Sergey Konkin <sergey.konkin.eth@gmail.com>
2022-09-30 08:23:57 +0300

    updated sway config

--- install.sh
+++ install.sh
@@ -15,24 +15,26 @@ link_dot() {
 }
 
 
-#link_dot .profile $HOME/.profile --backup
-#link_dot .bash_profile $HOME/.bash_profile --backup
-#link_dot .bashrc $HOME/.bashrc --backup
-#link_dot .bash.d $HOME/.bash.d
+## SHELL
+# link_dot .profile $HOME/.profile --backup
+# link_dot .bash_profile $HOME/.bash_profile --backup
+# link_dot .bashrc $HOME/.bashrc --backup
+# link_dot .bash.d $HOME/.bash.d
+# link_dot .ignore $HOME/.ignore
+# link_dot .inputrc $HOME/.inputrc
+# link_dot .vim $HOME/.vim
+# link_dot .config/git $HOME/.config/git
+# link_dot .config/starship.toml $HOME/.config/starship.toml
+# link_dot .config/alacritty $HOME/.config/alacritty
+# link_dot .config/fish $HOME/.config/fish
+# link_dot .config/nnn/plugins $HOME/.config/nnn/plugins
+# link_dot .config/bat $HOME/.config/bat
+# bat cache --build
 
-#link_dot .ignore $HOME/.ignore
-#link_dot .inputrc $HOME/.inputrc
-#link_dot .azotebg $HOME/.azotebg
-#link_dot .vim $HOME/.vim
-
-#link_dot .config/starship.toml $HOME/.config/starship.toml
-#link_dot .config/git $HOME/.config/git
-#link_dot .config/fish $HOME/.config/fish
-#link_dot .config/alacritty $HOME/.config/alacritty
-#link_dot .config/bat $HOME/.config/bat
-#bat cache --build
+## DESKTOP
+# link_dot .azotebg $HOME/.azotebg
 # link_dot .config/azote $HOME/.config/azote
 # link_dot .config/fastfetch $HOME/.config/fastfetch
-# link_dot .config/nnn/plugins $HOME/.config/nnn/plugins
 # link_dot .config/swaylock $HOME/.config/swaylock
-link_dot .config/waybar $HOME/.config/waybar
+# link_dot .config/waybar $HOME/.config/waybar
+link_dot .config/sway $HOME/.config/sway
