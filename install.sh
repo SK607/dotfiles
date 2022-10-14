e5a0b1931b533b40b0ac4ec3461bb9762be22035
Sergey Konkin <sergey.konkin.eth@gmail.com>
2022-10-14 13:19:31 +0300

    added conditional dotfile linking

--- install.sh
+++ install.sh
@@ -22,19 +22,47 @@ link_dot .bashrc "$HOME/.bashrc" --backup
 link_dot .bash.d "$HOME/.bash.d"
 link_dot .ignore "$HOME/.ignore"
 link_dot .inputrc "$HOME/.inputrc"
-link_dot .vim "$HOME/.vim"
-link_dot .config/git "$HOME/.config/git"
-link_dot .config/starship.toml "$HOME/.config/starship.toml"
-link_dot .config/alacritty "$HOME/.config/alacritty"
-link_dot .config/fish "$HOME/.config/fish"
-link_dot .config/nnn/plugins "$HOME/.config/nnn/plugins"
-link_dot .config/bat "$HOME/.config/bat"
-bat cache --build
+if test -x "$(command -v alacritty)"; then
+    link_dot .config/alacritty "$HOME/.config/alacritty"
+fi
+if test -x "$(command -v fish)"; then
+    link_dot .config/fish "$HOME/.config/fish"
+fi
+if test -x "$(command -v starship)"; then
+    link_dot .config/starship.toml "$HOME/.config/starship.toml"
+fi
+if test -x "$(command -v vim)"; then
+    link_dot .vim "$HOME/.vim"
+fi
+if test -x "$(command -v git)"; then
+    link_dot .config/git "$HOME/.config/git"
+fi
+if test -x "$(command -v nnn)"; then
+    link_dot .config/nnn/plugins "$HOME/.config/nnn/plugins"
+fi
+if test -x "$(command -v bat)"; then
+    link_dot .config/bat "$HOME/.config/bat"
+    bat cache --build
+fi
 
 ## DESKTOP
-link_dot .azotebg "$HOME/.azotebg"
-link_dot .config/azote "$HOME/.config/azote"
-link_dot .config/fastfetch "$HOME/.config/fastfetch"
-link_dot .config/swaylock "$HOME/.config/swaylock"
-link_dot .config/waybar "$HOME/.config/waybar"
-link_dot .config/sway "$HOME/.config/sway"
+if test -x "$(command -v azote)"; then
+    link_dot .azotebg "$HOME/.azotebg"
+    link_dot .config/azote "$HOME/.config/azote"
+fi
+if test -x "$(command -v fastfetch)"; then
+    link_dot .config/fastfetch "$HOME/.config/fastfetch"
+fi
+if test -x "$(command -v neofetch)"; then
+    link_dot .config/neofetch "$HOME/.config/neofetch"
+fi
+if test -x "$(command -v sway)"; then
+    link_dot .config/sway "$HOME/.config/sway"
+fi
+if test -x "$(command -v waybar)"; then
+    link_dot .config/waybar "$HOME/.config/waybar"
+fi
+if test -x "$(command -v swaylock)"; then
+    link_dot .config/swaylock "$HOME/.config/swaylock"
+fi
+
