753e91a8b98c4baf366e59f2de2fd583e12f030a
Sergey Konkin <sergey.konkin.eth@gmail.com>
2022-11-06 12:05:05 +0300

    added xorg support

--- .config/sway/scripts/terminal.sh
+++ .config/sway/scripts/terminal.sh
@@ -1,5 +1,10 @@
-WINDOW_PID=$(swaymsg -t get_tree | \
-	jq '.. | (.nodes? // empty)[] | select(.focused == true).pid? // empty')
+if [ "$XDG_SESSION_TYPE" = 'wayland' ]; then
+	WINDOW_PID="$(swaymsg -t get_tree | \
+		jq '.. | (.nodes? // empty)[] | select(.focused == true).pid? // empty')"
+else
+	WINDOW_PID="$(xdo pid)"
+fi
+
 [ -n "$WINDOW_PID" ] && TERM_PID="$(pgrep -oP $WINDOW_PID)"
 [ -n "$TERM_PID" ] && WD="$(readlink -e /proc/$TERM_PID/cwd)"
 ! [ -n "$WD" ] && WD="$HOME"
