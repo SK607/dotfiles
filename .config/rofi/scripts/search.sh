2a339c46466da7ea9f8ac6e6646097d475524eaf
Sergey Konkin <sergey.konkin.eth@gmail.com>
2022-11-11 22:40:34 +0300

    replaced double quotes with single

--- .config/rofi/scripts/search.sh
+++ .config/rofi/scripts/search.sh
@@ -3,12 +3,12 @@
 url() {
     query=$(echo "$*" | sed 's/[a-z]+://' | sed 's/\s+/+/g')
     case "$*" in
-        g:*) url="https://github.com/search?q=" ;;
-        y:*) url="https://www.youtube.com/results?search_query=" ;;
-        ya:*) url="https://yandex.ru/yandsearch?text=" ;;
-        go:*) url="https://www.google.com/search?q=" ;;
-        dd:*) url="https://www.duckduckgo.com/?q=" ;;
-        *) url="https://search.brave.com/search?source=desktop&q=" ;;
+        g:*) url='https://github.com/search?q=' ;;
+        y:*) url='https://www.youtube.com/results?search_query=' ;;
+        ya:*) url='https://yandex.ru/yandsearch?text=' ;;
+        go:*) url='https://www.google.com/search?q=' ;;
+        dd:*) url='https://www.duckduckgo.com/?q=' ;;
+        *) url='https://search.brave.com/search?source=desktop&q=' ;;
     esac
     echo "$url$query";
 }
